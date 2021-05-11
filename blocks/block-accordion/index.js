fb06519dc7a76ff308b93f01e97a25a3f670502d
Maros Vizina <maros.vizina@slovenskoit.sk>
2021-05-11 13:49:02 +0200

    SKIT-71-wp-block-accordion

---
+++ blocks/block-accordion/index.js
@@ -0,0 +1,214 @@
+/**
+ * BLOCK - accordion
+ *
+ * @link https://slovenskoit.sk
+ *
+ * @package WordPress
+ * @subpackage ID-SK
+ * @since ID-SK 1.0
+ */
+
+// Used to make item ids
+import { nanoid } from 'nanoid';
+const { registerBlockType } = wp.blocks; // the notation same as: import registerBlockType from wp.blocks;
+const {
+    RichText,
+    // InnerBlocks,
+    InspectorControls
+} = wp.blockEditor;
+const {
+    PanelBody,
+    PanelRow,
+    ToggleControl
+} = wp.components;
+const { Component } = wp.element
+const { __ } = wp.i18n;
+
+registerBlockType('idsk/accordion', {
+    // built-in attributes
+    title: __('Akordeón', 'idsk-toolkit'),
+    description: __('Zobrazuje skladací blok.', 'idsk-toolkit'),
+    icon: 'image-flip-vertical',
+    category: 'idsk-components',
+    keywords: [
+        __('accordion', 'idsk-toolkit'),
+        __('akordeón', 'idsk-toolkit'),
+        __('blok', 'idsk-toolkit'),
+    ],
+
+    // custom attributes
+    attributes: {
+        blockId: {
+            type: 'string'
+        },
+        items: {
+            type: 'array'
+        },
+    },
+
+    // The UI for the WordPress editor
+    edit: class Accordion extends Component {
+        constructor() {
+            super(...arguments)
+
+            // Match current state to saved quotes (if they exist)
+            this.state = {
+                items: this.props.attributes.items || [],
+            };
+
+            this.addItem = this.addItem.bind(this);
+            this.removeItem = this.removeItem.bind(this);
+            this.editItem = this.editItem.bind(this);
+        }
+
+        // adds empty placeholder for item
+        addItem(e) {
+            e.preventDefault()
+
+            // get items from state and their length
+            const { items } = this.state
+            // const length = items.length
+
+            // set up empty item
+            const emptyItem = {
+                id: nanoid(),
+                open: false,
+                title: '',
+                summary: '',
+                content: ''
+            }
+
+            // append new emptyItem object to items
+            const newItems = [...items, emptyItem]
+
+            // save new placeholder to WordPress
+            this.props.setAttributes({ items: newItems })
+
+            // and update state
+            return this.setState({ items: newItems })
+        }
+
+        // remove item
+        removeItem(e, index) {
+            e.preventDefault()
+
+            // make a true copy of items
+            const items = JSON.parse(JSON.stringify(this.state.items))
+
+            // remove specified item
+            items.splice(index, 1)
+
+            // save updated items and update state (in callback)
+            return (
+                this.props.setAttributes(
+                    { items: items },
+                    this.setState({ items: items })
+                )
+            )
+        };
+
+        // handler function to update item
+        editItem(key, index, value) {
+            // make a true copy of items
+            const items = JSON.parse(JSON.stringify(this.state.items))
+            if (items.length === 0) return
+
+            // update value
+            items[index][key] = value
+
+            // save values in WordPress and update state (in callback)
+            return (
+                this.props.setAttributes(
+                    { items: items },
+                    this.setState({ items: items })
+                )
+            )
+        };
+        
+        render() {
+            // Pull out the props we'll use
+            const { attributes, className, setAttributes } = this.props
+
+            // Pull out specific attributes for clarity below
+            const { blockId, items } = attributes
+
+            if (!blockId) {
+                this.props.setAttributes( { blockId: nanoid() } )
+            }
+
+            return <div className={className}>
+                <InspectorControls>
+                    <PanelBody title={__('Nastavenia akordeónu', 'idsk-toolkit')}>
+                        {!!items && items.map((item, index) => 
+                            <PanelRow>
+                                <ToggleControl
+                                    checked={item.open}
+                                    label={__('Akordeón', 'idsk-toolkit') + ' ' + (index+1)}
+                                    help={item.open ? __('Rozbalený', 'idsk-toolkit') : __('Zbalený', 'idsk-toolkit')}
+                                    onChange={checked => this.editItem('open', index, checked)}
+                                />
+                            </PanelRow>
+                        )}
+                    </PanelBody>
+                </InspectorControls>
+                <div class="govuk-accordion" data-module="govuk-accordion" id="with-descriptions">
+                    {!!items && items.map((item, index) =>
+                        <>
+                            <div key={item.id || index} class="govuk-accordion__section govuk-accordion__section--expanded">
+                                <div class="govuk-accordion__section-header">
+                                    <h2 class="govuk-accordion__section-heading">
+                                        <RichText
+                                            className="govuk-accordion__section-button"
+                                            key="editable"
+                                            tagName="span"
+                                            placeholder={__('Nadpis sekcie', 'idsk-toolkit')}
+                                            value={item.title}
+                                            onChange={value => this.editItem('title', index, value)} />
+                                        <span class="govuk-accordion__icon" aria-hidden="true"></span>
+                                    </h2>
+                                    <RichText
+                                        className="govuk-accordion__section-summary govuk-body"
+                                        key="editable"
+                                        tagName="div"
+                                        placeholder={__('Popis sekcie', 'idsk-toolkit')}
+                                        value={item.summary}
+                                        onChange={value => this.editItem('summary', index, value)} />
+                                </div>
+                                <div class="govuk-accordion__section-content">
+                                    <RichText
+                                        className="govuk-body section-content"
+                                        key="editable"
+                                        tagName="p"
+                                        placeholder={__('Obsah sekcie', 'idsk-toolkit')}
+                                        value={item.content}
+                                        onChange={value => this.editItem('content', index, value)} />
+                                        
+                                    {/* <InnerBlocks allowedBlocks={ALLOWED_BLOCKS} /> */}
+                                </div>
+                            </div>
+
+                            <button
+                                className="button-secondary button"
+                                type="submit"
+                                onClick={(e) => this.removeItem(e, index)}
+                            >{__('Vymazať akordeón', 'idsk-toolkit')}</button>
+                        </>
+                    )}
+
+                    <br/>
+                    <button
+                        className="button-primary button"
+                        type="submit"
+                        onClick={(e) => this.addItem(e)}
+                    >{__('Pridať akordeón', 'idsk-toolkit')}</button>
+
+                </div>
+            </div>;
+        }
+    },
+
+    // No save, dynamic block
+    save() {
+        return null
+    },
+})
--- blocks/block-accordion/index.php
+++ blocks/block-accordion/index.php
@@ -1,2 +1,94 @@
 <?php
-// Silence is golden.
+/**
+ * BLOCK - accordion - register dynamic block
+ *
+ * @link https://slovenskoit.sk
+ *
+ * @package WordPress
+ * @subpackage ID-SK
+ * @since ID-SK 1.0
+ */
+
+function idsktk_register_dynamic_accordion_block() {
+    // Only load if Gutenberg is available.
+    if (!function_exists('register_block_type')) {
+        return;
+    }
+
+    // Hook server side rendering into render callback
+    register_block_type('idsk/accordion', array(
+        'render_callback' => 'idsktk_render_dynamic_accordion_block'
+    ));
+}
+add_action('init', 'idsktk_register_dynamic_accordion_block');
+
+function idsktk_render_dynamic_accordion_block($attributes) {
+    // block attributes
+    $blockId = $attributes['blockId'];
+    $items = isset($attributes['items']) ? $attributes['items'] : array();
+  
+    ob_start(); // Turn on output buffering
+    ?>
+
+    <div class="govuk-accordion" data-module="govuk-accordion" id="<?php echo $blockId; ?>">
+        <?php foreach ($items as $key => $item) { ?>
+            <div class="govuk-accordion__section <?php echo $item['open'] ? 'govuk-accordion__section--expanded' : '' ?>">
+                <div class="govuk-accordion__section-header">
+                    <h2 class="govuk-accordion__section-heading">
+                        <span class="govuk-accordion__section-button" id="<?php echo $blockId.'-heading-'.$key ?>">
+                            <?php echo $item['title']; ?>
+                        </span>
+                    </h2>
+                    <div class="govuk-accordion__section-summary govuk-body" id="<?php echo $blockId.'-summary-'.$key ?>">
+                        <?php echo $item['summary']; ?>
+                    </div>
+                </div>
+                <div id="<?php echo $blockId.'-content-'.$key ?>" class="govuk-accordion__section-content" aria-labelledby="<?php echo $blockId.'-heading-'.$key ?>">
+                    <?php echo $item['content']; ?>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+<?php /*
+    <div class="govuk-clearfix"></div>
+    <div data-module="idsk-accordion" class="<?php echo $className; ?>">
+        <?php for ($i=0; $i < $number_of_cols; $i++) { ?>
+            <div class="idsk-accordion idsk-accordion-<?php echo $number_of_cols; ?>">
+                <?php foreach ($items as $key => $item) { ?>
+                    <?php if ($number_of_cols == 1 || round(($key / $number_of_items)) == $i) { ?>
+                        <?php if ($hide_tiles) {
+                            $aria = ($key > 3 && $number_of_cols == 2 && $i == 0) || ($key > ((($number_of_items / $number_of_cols)) + ($number_of_showed_tiles - 1)) && $number_of_cols == 2 && $i == 1) || ($key > 4 && $number_of_cols == 1) ? 'true' : 'false';
+                            $classes = ($key > 4) ? 'idsk-accordion__item--two-columns-hide-mobile' : '';
+                            $classes .= (
+                                ($key > 4 && $number_of_cols == 2 && $i == 0)
+                                || ($key > (($number_of_items / $number_of_cols) + $number_of_showed_tiles - 1) && $number_of_cols == 2 && $i == 1) 
+                                || ($key > 4 && $number_of_cols == 1)
+                                ) ? ' idsk-accordion__item--two-columns-hide' : '';
+                        } ?>
+                        <div class="idsk-accordion__item <?php echo $classes; ?>">
+                            <a href="<?php echo $item['link']; ?>" class="idsk-accordion-title" title="<?php echo $item['title']; ?>" aria-hidden="<?php echo $aria; ?>">
+                                <?php echo $item['title']; ?>
+                            </a>
+                            <p class="idsk-accordion-subtitle" aria-hidden="<?php echo $aria; ?>"><?php echo $item['subtitle']; ?></p>
+                            <hr class="idsk-accordion-line" aria-hidden="true"/>
+                        </div>
+                    <?php } ?>
+                <?php } ?>
+            </div>
+        <?php } ?>
+        <?php if ($hide_tiles) { ?>
+            <?php $classes = ($number_of_items < 6 && $number_of_cols) || ($number_of_items < 11 && !$number_of_cols) ? ' idsk-accordion__uncollapse-hide--desktop' : ''; ?>
+            <?php $classes = $classes . ($number_of_items < 6 ? ' idsk-accordion__uncollapse-hide--mobile' : ''); ?>
+        <div class="govuk-grid-column-full idsk-accordion__collapse--shadow idsk-accordion__uncollapse-div<?php echo $classes; ?>">
+            <button id="idsk-accordion__uncollapse-button" class="idsk-accordion__colapse--button" type="button" data-line1="<?php echo __( 'Zobraziť viac', 'idsk-toolkit' ); ?>" data-line2="<?php echo __( 'Zobraziť menej', 'idsk-toolkit' ); ?>"><?php echo __( 'Zobraziť viac', 'idsk-toolkit' ); ?></button>
+        </div>
+        <?php } ?>
+    </div> */ ?>
+  
+    <?php
+      /* END HTML OUTPUT */
+    $output = ob_get_contents(); // collect output
+    ob_end_clean(); // Turn off ouput buffer
+  
+    return $output; // Print output
+  }
