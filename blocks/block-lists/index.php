ec027067942204675b589c15c9db7475e5872f74
Maros Vizina <maros.vizina@slovenskoit.sk>
2021-05-19 13:44:29 +0200

    SKIT-81-wp-block-lists - cleanup

--- blocks/block-lists/index.php
+++ blocks/block-lists/index.php
@@ -23,8 +23,6 @@ function idsktk_register_dynamic_lists_block() {
 add_action('init', 'idsktk_register_dynamic_lists_block');
 
 function render_items($items, $parent=null) {
-    // $output = '';
-
     foreach ($items as $key => $item) {
         if ($item['parent'] == $parent) { ?>
             <li><?php echo $item['text'] ?></li>
@@ -36,6 +34,7 @@ function render_items($items, $parent=null) {
                 <<?php echo $subType ?> class="<?php echo "govuk-list ".$subListType ?>">
                     <?php echo render_items($items, $item['id']) ?>
                 </<?php echo $subType ?>>
+
             <?php } ?>
 
         <?php
@@ -43,63 +42,6 @@ function render_items($items, $parent=null) {
             continue;
         }
     }
-
-
-
-/*
-    foreach ($items as $key => $item) {
-        echo $index.'-'.$key.'-'.($key+1).'<br/>';
-        echo '<pre>';
-        var_dump($item);
-        echo '</pre>';
-        if ($item['parent'] == $index) { ?>
-            <li><?php echo $item['text'] ?></li>
-
-            <?php if ($item['hasItems'] == true) { 
-                $subListType = $item['listType'];
-                $subType = strpos($subListType, 'number') ? 'ol' : 'ul';
-            ?>
-                <<?php echo $subType ?> class="<?php echo "govuk-list ".$subListType ?>">
-                    <?php echo render_items($items, ($key+1)) ?>
-                </<?php echo $subType ?>>
-            <?php } ?>
-
-        <?php
-        } else {
-            continue;
-            // break;
-        }
-
-
-
-
-
-        // if ($key >= $index) {
-        //     $output .= '<li>'.$item['text'].'</li>';
-
-        //     $listType = ''.$item['listType'];
-        //     // echo $key.' - '.$listType.'<br/>';
-
-        //     if ($item['hasItems'] == true && $item['parent'] == $index) { 
-        //         $type = strpos($listType, 'number') ? 'ol' : 'ul';
-
-        //         $itemList = render_items($items, $key+1);
-
-        //         // echo '<hr>';
-        //         // echo $itemList;
-        //         // echo '<hr>';
-
-        //         // $output .= '<'.$type.' class="govuk-list '.$listType.'">';
-        //         // $output .= render_items($items, $key+1);
-        //         // $output .= '</'.$type.'>';
-
-        //     }
-        // } else {
-        //     continue;
-        // }
-    }*/
-
-    // echo $output;
 }
 
 function idsktk_render_dynamic_lists_block($attributes) {
@@ -110,47 +52,10 @@ function idsktk_render_dynamic_lists_block($attributes) {
     $type = mb_strpos($listType, 'number') ? 'ol' : 'ul';
 
     ob_start(); // Turn on output buffering
-
-    // echo '
-    //     <'.$type.' class="govuk-list '.$listType.'">
-
-    //     </'.$type.'>
-    // ';
-
-
-
     ?>
 
     <<?php echo $type; ?> class="<?php echo "govuk-list ".$listType; ?>">
-
-        <?php
-            echo render_items($items); 
-/*
-            foreach ($items as $key => $item) { ?>
-
-                <li><?php echo $item['text'] ?></li>
-
-            <?php
-
-                
-                // echo '<pre>';
-                // var_dump($item);
-                // echo '</pre>';
-
-                $subListType = $item['listType'];
-
-                if ($item['hasItems'] == true) { 
-                    $subType = strpos($subListType, 'number') ? 'ol' : 'ul';
-                ?>
-                    <<?php echo $subType ?> class="<?php echo "govuk-list ".$subListType ?>">
-                        <?php echo render_items($items, $key) ?>
-                    </<?php echo $subType ?>>
-
-                <?php
-                }
-            }*/
-        ?>
-
+        <?php echo render_items($items); ?>
     </<?php echo $type; ?>>
 
     <?php
