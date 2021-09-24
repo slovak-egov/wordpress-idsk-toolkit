c34c85e74a465578d2a6db11e5b94cb6b0f50131
Maros Vizina <maros.vizina@slovenskoit.sk>
2021-09-24 11:50:22 +0200

    SKITRP-136 - updated towards IDSK v2.7.0 + button component fix

--- blocks/block-intro/index.php
+++ blocks/block-intro/index.php
@@ -29,7 +29,6 @@ function idsktk_render_dynamic_intro_block($attributes) {
     $search = isset($attributes['withSearch']) && $attributes['withSearch'] ? $attributes['withSearch'] : FALSE;
     $searchTitle = isset($attributes['searchTitle']) ? $attributes['searchTitle'] : '';
     $sideStyle = isset($attributes['sideStyle']) ? $attributes['sideStyle'] : '';
-    $defaultStyle = $sideStyle == 'default' || $sideStyle == '' ? true : false;
     $sideTitle = isset($attributes['sideTitle']) ? $attributes['sideTitle'] : '';
 
     $url1 = isset($attributes['url1']) ? $attributes['url1'] : '';
@@ -52,28 +51,14 @@ function idsktk_render_dynamic_intro_block($attributes) {
     $sideList = array();
     foreach($sideContent->getElementsByTagName('li') as $node)
     {
-        if ($defaultStyle) {
-            $node->setAttribute('class', 'idsk-intro-block__side-menu__default__li');
-            $link = $sideContent->createElement('p');
-
-            while ($node->firstChild) {
-                $link->appendChild($node->firstChild);
-            }
-
-            $node->appendChild($link);
-        } else {
-            $node->setAttribute('class', 'idsk-intro-block__side-menu__li');
-        }
+        $node->setAttribute('class', 'idsk-intro-block__side-menu__li');
 
         foreach($node->getElementsByTagName('a') as $href)
         {
-            if ($defaultStyle) {
-                $href->setAttribute('class', 'govuk-link');
-            } else {
-                $href->setAttribute('class', 'idsk-intro-block__side-menu__a');
-            }
+            $href->setAttribute('class', 'govuk-link idsk-intro-block__side-menu__a');
+            $href->setAttribute('title', $href->textContent);
         }
-        
+
         $sideList[] = $sideContent->saveHTML($node);
     }
 
@@ -84,24 +69,29 @@ function idsktk_render_dynamic_intro_block($attributes) {
         <div class="idsk-intro-block ">
             <div class="govuk-grid-row ">
                 <div class="govuk-grid-column-full govuk-grid-column-two-thirds-from-desktop">
-                    <h2 class="govuk-heading-xl">
+                    <h2 class="govuk-heading-m">
                         <?php echo $title ?>
                     </h2>
-                    <p class="idsk-intro-block__subtitle govuk-caption-l"><?php echo $subTitle ?></p>
+                    <p class="govuk-body"><?php echo $subTitle ?></p>
                     <?php if ($search) { ?>
                         <form role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
                             <div data-module="idsk-search-component" class="idsk-search-component">
                                 <label class="" for="intro-block-search"><?php esc_html_e('Zadajte hľadaný výraz', 'idsk-toolkit') ?></label>
                                 <input class="govuk-input govuk-input--width-30 idsk-search-component__input " id="intro-block-search" name="s" type="search" value="<?php echo get_search_query(); ?>">
                                 <button type="submit" class="govuk-button idsk-search-component__button ">
-                                    <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M21.0115 13.103C21.0115 17.2495 17.5484 20.6238 13.2928 20.6238C9.03714 20.6238 5.57404 17.2495 5.57404 13.103C5.57404
+                                    <svg width="18" height="18" viewbox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                        d="M21.0115 13.103C21.0115 17.2495 17.5484 20.6238 13.2928 20.6238C9.03714 20.6238 5.57404 17.2495 5.57404 13.103C5.57404
                                             8.95643 9.03714 5.58212 13.2928 5.58212C17.5484 5.58212 21.0115 8.95643 21.0115 13.103ZM29.833 27.0702C29.833 26.4994
                                             29.5918 25.9455 29.1955 25.5593L23.2858 19.8012C24.6814 17.8371 25.4223 15.4868 25.4223 13.103C25.4223 6.57259 19.995
                                             1.28451 13.2928 1.28451C6.59058 1.28451 1.16333 6.57259 1.16333 13.103C1.16333 19.6333 6.59058 24.9214 13.2928
                                             24.9214C15.7394 24.9214 18.1515 24.1995 20.1673 22.8398L26.077 28.5811C26.4732 28.984 27.0418 29.219 27.6276
-                                            29.219C28.8337 29.219 29.833 28.2453 29.833 27.0702Z" fill="white"></path>
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.75708 13.103C0.75708 6.35398 6.36621 0.888672 13.2928 0.888672C20.2194 0.888672 25.8285 6.35398 25.8285
+                                            29.219C28.8337 29.219 29.833 28.2453 29.833 27.0702Z"
+                                        fill="white"/>
+                                        <path
+                                        fill-rule="evenodd"
+                                        clip-rule="evenodd"
+                                        d="M0.75708 13.103C0.75708 6.35398 6.36621 0.888672 13.2928 0.888672C20.2194 0.888672 25.8285 6.35398 25.8285
                                             13.103C25.8285 15.4559 25.1301 17.7778 23.8094 19.7516L29.4827 25.2794C29.9551 25.7396 30.2392 26.3943 30.2392
                                             27.0702C30.2392 28.464 29.058 29.6149 27.6276 29.6149C26.9347 29.6149 26.2611 29.3385 25.787 28.8584L20.1168
                                             23.3497C18.0909 24.6367 15.7078 25.3172 13.2928 25.3172C6.36621 25.3172 0.75708 19.8519 0.75708 13.103ZM13.2928
@@ -112,7 +102,8 @@ function idsktk_render_dynamic_intro_block($attributes) {
                                             5.97796C9.26151 5.97796 5.98029 9.17504 5.98029 13.103C5.98029 17.0309 9.26151 20.228 13.2928 20.228C17.3241 20.228
                                             20.6053 17.0309 20.6053 13.103C20.6053 9.17504 17.3241 5.97796 13.2928 5.97796ZM5.16779 13.103C5.16779 8.73781 8.81278
                                             5.18629 13.2928 5.18629C17.7728 5.18629 21.4178 8.73781 21.4178 13.103C21.4178 17.4681 17.7728 21.0196 13.2928
-                                            21.0196C8.81278 21.0196 5.16779 17.4681 5.16779 13.103Z" fill="white"></path>
+                                            21.0196C8.81278 21.0196 5.16779 17.4681 5.16779 13.103Z"
+                                        fill="white"/>
                                     </svg>
                                     <span class="govuk-visually-hidden"><?php esc_html_e('Vyhľadávanie', 'idsk-toolkit') ?></span>
                                 </button>
@@ -121,7 +112,7 @@ function idsktk_render_dynamic_intro_block($attributes) {
 
                         <div>
                             <ul class="idsk-intro-block__list__ul">
-                                <li class="idsk-intro-block__bottom-menu__li govuk-caption-l">
+                                <li class="idsk-intro-block__bottom-menu__li govuk-caption-m">
                                     <span><?php echo $searchTitle ?></span>
                                 </li>
                                 
@@ -168,23 +159,17 @@ function idsktk_render_dynamic_intro_block($attributes) {
                         </div>
                     <?php } ?>
                 </div>
-                <div class="<?php echo !$defaultStyle ? 'idsk-intro-block__side-menu' : '' ?>">
-                    <div id="<?php echo $defaultStyle ? 'idsk-intro-block__side-menu__default' : 'idsk-intro-block__side-menu__title' ?>" class='govuk-grid-column-full govuk-grid-column-one-third-from-desktop <?php echo $sideStyle ?>'>
-                        <h2 class="govuk-heading-l">
-                            <?php echo $sideTitle ?>
-                        </h2>
-                        <?php echo $defaultStyle ? '<div class="idsk-intro-block__side-menu__default__subtitle">' : '' ?>
-                            <ul class="<?php echo $defaultStyle ? 'idsk-intro-block__side-menu__default__ul' : 'idsk-intro-block__side-menu__ul' ?>">
-                            
-                                <?php 
-                                foreach ($sideList as $key => $val) {
-                                    echo $val;
-                                }
-                                ?>
-
-                            </ul>
-                        <?php echo $defaultStyle ? '</div>' : '' ?>
-                    </div>
+                <div class="idsk-intro-block__side-menu govuk-grid-column-full govuk-grid-column-one-third-from-desktop <?php echo $sideStyle ?>">   
+                    <h2 class="govuk-heading-s">
+                        <?php echo $sideTitle ?>
+                    </h2>
+                    <ul class="idsk-intro-block__side-menu__ul">
+                        <?php 
+                        foreach ($sideList as $key => $val) {
+                            echo $val;
+                        }
+                        ?>
+                    </ul>
                 </div>
             </div>
         </div>
